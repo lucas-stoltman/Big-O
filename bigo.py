2eec3d8e43dfd68da92d8cacdd697af375329302
RainyKnight <stoltmanlucas@gmail.com>
2022-04-18 09:21:49 -0700

    Implement tests

--- bigo.py
+++ bigo.py
@@ -3,12 +3,9 @@
 # April 16th, 2022
 # CSS 340 A, Dimpsey
 # Version: 1.0
-import copy
 
 
 class BigO:
-    import copy
-
     # The unsorted list is searched linearly to see if the val is in the list
     @staticmethod
     def find1(self, val: int):
@@ -17,14 +14,15 @@ class BigO:
                 return True
         return False
 
-    # TODO
-    # A deep copy is made of the list; the copied list is then sorted using
-    # the "sort" built- in function and then a binary search is performed on
-    # the list to find if the val is in the list
-    def find2(self, val: int):
-        result = copy.deepcopy(self)
-
-        return True
+    # # TODO
+    # # A deep copy is made of the list; the copied list is then sorted using
+    # # the "sort" built- in function and then a binary search is performed on
+    # # the list to find if the val is in the list
+    # @staticmethod
+    # def find2(self, val: int):
+    #     result = copy.deepcopy(self)
+    #
+    #     return True
 
     #
     # TODO
--- main.py
+++ main.py
@@ -9,6 +9,24 @@ from bigo import BigO
 list1 = [5, 2, 5, 6, 7, 5, 2]
 
 test1 = BigO()
-print(test1.find1(list1, 6))
 
-print(test1.find1(list1, 6))
+val1 = 7
+val2 = 3
+
+print("\n---", "find1", "---", )
+print(f"{val1} : " + str(test1.find1(list1, val1)))
+print(f"{val2} : " + str(test1.find1(list1, val2)))
+
+# TODO
+# print("\n---", "find2", "---", )
+# print(f"{val1} : " + str(test1.find2(list1, val1)))
+# print(f"{val2} : " + str(test1.find2(list1, val2)))
+
+print("\n---", "find3", "---", )
+print(f"{val1} : " + str(test1.find3(list1, val1)))
+print(f"{val2} : " + str(test1.find3(list1, val2)))
+
+# TODO
+# print("\n---", "find4", "---", )
+# print(f"{val1} : " + str(test1.find4(list1, val1)))
+# print(f"{val2} : " + str(test1.find4(list1, val2)))
