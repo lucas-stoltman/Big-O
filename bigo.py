56b838b0c4214c8363c7aee53829c5967d858572
RainyKnight <stoltmanlucas@gmail.com>
2022-04-18 09:09:20 -0700

    Fix find1()

--- bigo.py
+++ bigo.py
@@ -3,23 +3,36 @@
 # April 16th, 2022
 # CSS 340 A, Dimpsey
 # Version: 1.0
+import copy
+
 
 class BigO:
+    import copy
 
     # The unsorted list is searched linearly to see if the val is in the list
-    def find1(self, list_input: list, val: int):
-        print(val in list_input)
+    @staticmethod
+    def find1(self, val: int):
+        for i in self:
+            if i == val:
+                return True
+        return False
 
-    # # A deep copy is made of the list; the copied list is then sorted using
-    # # the "sort" built- in function and then a binary search is performed on
-    # # the list to find if the val is in the list
-    # def find2(self, list_input: list, val: int = 0):
-    #     return True
+    # TODO
+    # A deep copy is made of the list; the copied list is then sorted using
+    # the "sort" built- in function and then a binary search is performed on
+    # the list to find if the val is in the list
+    def find2(self, val: int):
+        result = copy.deepcopy(self)
+
+        return True
+
     #
-    # # The "in" built-in is used to determine if the val is in the unsorted list
-    # def find3(self, list_input: list, val: int = 0):
-    #     return True
+    # TODO
+    # The "in" built-in is used to determine if the val is in the unsorted list
+    def find3(self, val: int):
+        print(val in self)
     #
+    # # TODO
     # # This function requires the list to be sorted before it is called. A binary search is
     # # performed on the pre-sorted list to find val.
     # def find4(self, list_input: list, val: int = 0):
