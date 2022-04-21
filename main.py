cfdd64bfe49cc5f042a22c193950c8a323f68363
RainyKnight <stoltmanlucas@gmail.com>
2022-04-20 22:33:02 -0700

    Add timer functions to TCs

--- main.py
+++ main.py
@@ -5,8 +5,14 @@
 # Version: 1.0
 
 from bigo import BigO
+import time
+
+print("---------------------------")
+print("Beginning Testing")
+print("---------------------------")
 
 list1 = [5, 2, 5, 6, 7, 5, 2]
+print(f"list1: {list1}")
 
 test1 = BigO()
 
@@ -14,18 +20,30 @@ val1 = 7
 val2 = 3
 
 print("\n---", "find1", "---", )
+tic = time.perf_counter_ns()
 print(f"{val1} : " + str(test1.find1(list1, val1)))
 print(f"{val2} : " + str(test1.find1(list1, val2)))
+toc = time.perf_counter_ns()
+print(f"{toc - tic} nanoseconds")
 
 print("\n---", "find2", "---", )
+tic = time.perf_counter_ns()
 print(f"{val1} : " + str(test1.find2(list1, val1)))
 print(f"{val2} : " + str(test1.find2(list1, val2)))
+toc = time.perf_counter_ns()
+print(f"{toc - tic} nanoseconds")
 
 print("\n---", "find3", "---", )
+tic = time.perf_counter_ns()
 print(f"{val1} : " + str(test1.find3(list1, val1)))
 print(f"{val2} : " + str(test1.find3(list1, val2)))
+toc = time.perf_counter_ns()
+print(f"{toc - tic} nanoseconds")
 
 # TODO
 # print("\n---", "find4", "---", )
+# tic = time.perf_counter_ns()
 # print(f"{val1} : " + str(test1.find4(list1, val1)))
 # print(f"{val2} : " + str(test1.find4(list1, val2)))
+# toc = time.perf_counter_ns()
+# print(f"{toc - tic} nanoseconds")
