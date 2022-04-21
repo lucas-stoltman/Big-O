69dabe627bcc0912126a2dd8d5effece5e017dfe
RainyKnight <stoltmanlucas@gmail.com>
2022-04-20 22:16:30 -0700

    Update testing for find2()

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ print("\n---", "find1", "---", )
 print(f"{val1} : " + str(test1.find1(list1, val1)))
 print(f"{val2} : " + str(test1.find1(list1, val2)))
 
-# # TODO
-# print("\n---", "find2", "---", )
-# print(f"{val1} : " + str(test1.find2(list1, val1)))
-# print(f"{val2} : " + str(test1.find2(list1, val2)))
+print("\n---", "find2", "---", )
+print(f"{val1} : " + str(test1.find2(list1, val1)))
+print(f"{val2} : " + str(test1.find2(list1, val2)))
 
 print("\n---", "find3", "---", )
 print(f"{val1} : " + str(test1.find3(list1, val1)))
