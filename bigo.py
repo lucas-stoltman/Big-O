c96642c61fce1bb1d0ee12bac355176b91ee4562
RainyKnight <stoltmanlucas@gmail.com>
2022-04-21 03:09:58 -0700

    Implement find4()

--- bigo.py
+++ bigo.py
@@ -8,6 +8,7 @@ from bisect import bisect_right
 
 
 class BigO:
+
     # The unsorted list is searched linearly to see if the val is in the list
     @staticmethod
     def find1(self: list, val: int):
@@ -16,11 +17,12 @@ class BigO:
                 return True
         return False
 
-    # # A deep copy is made of the list; the copied list is then sorted using
-    # # the "sort" built- in function and then a binary search is performed on
-    # # the list to find if the val is in the list
+    # A deep copy is made of the list; the copied list is then sorted using
+    # the "sort" built- in function and then a binary search is performed on
+    # the list to find if the val is in the list
     @staticmethod
     def find2(self: list, val: int):
+
         # create a deep copy
         deep_copy = copy.deepcopy(self)
 
@@ -29,35 +31,23 @@ class BigO:
 
         # binary search
         i = bisect_right(deep_copy, val)
-        if i != 0 and deep_copy[i-1] == val:
+        if i != 0 and deep_copy[i - 1] == val:
             return True
         else:
             return False
 
-        # low = 0
-        # high = len(self) - 1
-        # mid = 0
-        # while low <= high:
-        #     mid = (high + low) // 2
-        #     # check the left
-        #     if self[mid] < val:
-        #         low = mid + 1
-        #     #
-        #     elif self[mid] > val:
-        #         high = mid - 1
-        #     else:
-        #         return True
-        # return False
-
-    #
-    # TODO find3()
     # The "in" built-in is used to determine if the val is in the unsorted list
     @staticmethod
     def find3(self: list, val: int):
         return val in self
-    #
-    # # TODO find4()
-    # # This function requires the list to be sorted before it is called. A binary search is
-    # # performed on the pre-sorted list to find val.
-    # def find4(self: list, val: int = 0):
-    #     return True
+
+    # This function requires the list to be sorted before it is called. A binary search is
+    # performed on the pre-sorted list to find val.
+    @staticmethod
+    def find4(self: list, val: int = 0):
+        # binary search
+        i = bisect_right(self, val)
+        if i != 0 and self[i - 1] == val:
+            return True
+        else:
+            return False
