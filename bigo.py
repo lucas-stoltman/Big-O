6fe28cd797299b58cebb15cad62f78392ae60478
RainyKnight <stoltmanlucas@gmail.com>
2022-04-28 21:41:09 -0700

    Change name of list parameter self -> arr

--- bigo.py
+++ bigo.py
@@ -8,8 +8,8 @@ from bisect import bisect_right
 
 
 # The unsorted list is searched linearly to see if the val is in the list
-def find1(self: list, val: int):
-    for i in self:
+def find1(arr: list, val: int):
+    for i in arr:
         if i == val:
             return True
     return False
@@ -18,9 +18,9 @@ def find1(self: list, val: int):
 # A deep copy is made of the list; the copied list is then sorted using
 # the "sort" built- in function and then a binary search is performed on
 # the list to find if the val is in the list
-def find2(self: list, val: int):
+def find2(arr: list, val: int):
     # create a deep copy
-    deep_copy = copy.deepcopy(self)
+    deep_copy = copy.deepcopy(arr)
 
     # sort built-in function
     deep_copy.sort()
@@ -34,16 +34,16 @@ def find2(self: list, val: int):
 
 
 # The "in" built-in is used to determine if the val is in the unsorted list
-def find3(self: list, val: int):
-    return val in self
+def find3(arr: list, val: int):
+    return val in arr
 
 
 # This function requires the list to be sorted before it is called. A binary search is
 # performed on the pre-sorted list to find val.
-def find4(self: list, val: int = 0):
+def find4(arr: list, val: int = 0):
     # binary search
-    i = bisect_right(self, val)
-    if i != 0 and self[i - 1] == val:
+    i = bisect_right(arr, val)
+    if i != 0 and arr[i - 1] == val:
         return True
     else:
         return False
